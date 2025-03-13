fb0e1fc3a5b3b871812af5981cd662f11a1afa1e
avdavid57 <avdavid57@gmail.com>
2025-03-12 19:48:37 -0700

    Completed calculator functions

--- calculator.js
+++ calculator.js
@@ -1,10 +1,58 @@
-function operate() {
+// Set variables for each number and the selected operator
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+let isOperatorClicked = false;
 
-    let firstNumber;
-    let operator;
-    let secondNumber;
-    let calculatedValue = 0;
+// Select the buttons
+const buttons = document.querySelectorAll(".calculator-button");
+const screen = document.getElementById("textbox-container");
 
+// Add event listeners to each button
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const value = button.textContent;
+
+        // check value is a number
+        if (!isNaN(value) || value === ".") {
+            if (isOperatorClicked) {
+                secondNumber += value;
+            } else {
+                firstNumber += value;
+            }
+        } else if (["+", "-", "*", "/"].includes(value)) {
+            // check if operator is clicked and firstNumber <> ""
+            if (firstNumber != "") {
+                operator = value;
+                isOperatorClicked = true;
+            }
+        } else if (value == "=") {
+            // if equals is clicked
+            if (firstNumber !== "" && operator !== "" && secondNumber !== "") {
+                const result = operate(parseFloat(firstNumber), operator,parseFloat(secondNumber));
+                screen.textContent = result;
+                firstNumber = screen.textContent;
+                operator = "";
+                secondNumber = "";
+                isOperatorClicked = false;
+                return;
+            }
+        } else if (value == "Clear") {
+            firstNumber = "";
+            secondNumber = "";
+            operator = "";
+            isOperatorClicked = false;
+            screen.textContent = "";
+            return;
+        }
+
+        screen.textContent = firstNumber + (operator ? " " + operator + " " : "") + secondNumber;
+    });
+});
+
+function operate(firstNumber, operator, secondNumber) {
+    let calculatedValue;
+    
     switch (operator) {
         case "+":
             calculatedValue = add(firstNumber, secondNumber);
@@ -18,9 +66,10 @@ function operate() {
         case "/":
             calculatedValue = divide(firstNumber, secondNumber);
             break;
+        default: calculatedValue = 0;
     }
 
-    console.log(calculatedValue);
+    return calculatedValue;
 };
 
 function add(firstNumber, secondNumber) {
